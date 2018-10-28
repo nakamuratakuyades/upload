b1c934e6f27a1ce8fb55fd0778300a8cef7676a2
中村拓哉 <volleyball.lucky7@gmail.com>
2018-10-28 21:13:02 +0900

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
+  resources :contacts
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :favorites, only: [:create, :destroy]
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
 
   get '/', to: 'tops#index'
   get '/tops', to: 'tops#index'
   get '/contacts', to: 'blogs#contact'
   get '/login', to: 'sessions#new'
 
-  resources :blogs do
-    collection do
-      post :confirm
-    end
-  end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
 end
